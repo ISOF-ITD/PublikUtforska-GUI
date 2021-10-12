5ea9dfb55ecc38633412d421a0e1eab460446fba
Per Låås <per.laas@isof.se>
2021-10-12 15:36:22 +0200

    Changed bookmark term to "favoriter"

--- scripts/lang/langData.js
+++ scripts/lang/langData.js
@@ -1,13 +1,13 @@
 export default {
 	'sv': {
 		// LocalLibraryView Local storage my "records":
-		'Mina sägner': 'Mina sparade dokument',
-		'Inga sparade sägner': 'Inga sparade dokument',
-		'Klicka på stjärnan för att spara sägner till din egen lista.': 'Klicka på stjärnan för att spara dokument till din egen lista.',
-		'har sparats till dina sägner': 'har sparats till dina dokument',
-		'Klicka här för att visa lista över dina sparade uppteckningar.': 'Klicka här för att visa lista över dina sparade dokument.',
-		'Samtliga uppteckningar från orten': 'Samtliga dokument från orten',
-		'Länk till dina sägner har kopierats.': 'Länk till ditt dokument har kopierats.',
+		'Mina sägner': 'Mina sparade favoriter',
+		'Inga sparade sägner': 'Inga sparade favoriter',
+		'Klicka på stjärnan för att spara sägner till din egen lista.': 'Klicka på stjärnan för att spara favoriter till din egen lista.',
+		'har sparats till dina sägner': 'har sparats till dina favoriter',
+		'Klicka här för att visa lista över dina sparade uppteckningar.': 'Klicka här för att visa lista över dina sparade favoriter.',
+		'Samtliga uppteckningar från orten': 'Samtliga favoriter från orten',
+		'Länk till dina sägner har kopierats.': 'Länk till ditt favorit har kopierats.',
 		//Temporary solution to remove text "Fylke" for Finland (and this solution also Norway)
 		'Fylke': ' ',
 		// Transcription status to user:
