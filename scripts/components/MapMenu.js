4ab3c8a76344556724b2215b7aed793a40509d7b
Rico Simke <rico.simke@isof.se>
2023-02-21 13:28:51 +0100

    convert MapMenu to functional component

--- scripts/components/MapMenu.js
+++ scripts/components/MapMenu.js
@@ -1,54 +1,28 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import routeHelper from './../utils/routeHelper'
-
 import SearchBox from './SearchBox';
-//import NordicSwitch from './NordicSwitch';
 import FilterSwitch from './FilterSwitch';
 
-export default class MapMenu extends React.Component {
-	constructor(props) {
-		super(props);
+export default function MapMenu(props) {
+  const searchParams = (pathname) => {
+    return routeHelper.createParamsFromSearchRoute(pathname.split(props.match.url)[1]);
+  };
 
-		this.state = {
-			selectedCategory: null,
-			// expanded: false,
-			advanced: false,
-		};
-	}
-
-	searchBoxSearchHandler(event) {
-		this.setState({
-			searchValue: event.searchValue,
-			searchField: event.searchField,
-		}, function() {
-			this.updateRoute();
-		}.bind(this));
-	}
-
-	render() {
-		// used for the unique key in searchBox
-		return (
-			<Route
-			path={['/places/:place_id([0-9]+)?', '/records/:record_id', '/person/:person_id']}
-				render= {(props) =>
-					<div className={'menu-wrapper'+(this.props.expanded ? ' menu-expanded' : '')+(this.state.advanced ? ' advanced-menu-view' : '')}>
-
-						<FilterSwitch 
-							searchParams={routeHelper.createParamsFromSearchRoute(props.location.pathname.split(props.match.url)[1])}
-							{...props} 
-						/>
-
-						<SearchBox
-							key={props.location.pathname.split(props.match.url)[1]}
-							searchParams={routeHelper.createParamsFromSearchRoute(props.location.pathname.split(props.match.url)[1])}
-							expanded={this.props.expanded}
-							{...props}	
-						/>
-
-					</div>
-				}
-			/>
-		);
-	}
+  return (
+    <Route path={['/places/:place_id([0-9]+)?', '/records/:record_id', '/person/:person_id']}>
+      {(routeProps) => (
+        <div className={`menu-wrapper${props.expanded ? ' menu-expanded' : ''}`}>
+          <FilterSwitch {...routeProps} searchParams={searchParams(routeProps.location.pathname)} />
+          <SearchBox
+		  	{...routeProps}
+			// the search field value does not update when the route changes
+			// so we need to force it to update with the key prop
+			key={routeProps.location.pathname}
+			searchParams={searchParams(routeProps.location.pathname)}
+			expanded={props.expanded} />
+        </div>
+      )}
+    </Route>
+  );
 }
