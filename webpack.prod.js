0788431e9075874ad45aa17b9abe4a3aa5886baf
Rico Simke <rico.simke@isof.se>
2023-02-21 08:25:17 +0100

    tells webpack to split the code into chunks

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,4 +9,12 @@ module.exports = merge(common, {
 
   mode: 'production',
 
+  // tells webpack to split the code
+  // into multiple files
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    }
+  },
+
 });
