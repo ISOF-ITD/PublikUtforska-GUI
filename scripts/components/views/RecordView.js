d11243cf204ec6f1339dab51189808e12f977c5a
Rico Simke <rico.simke@isof.se>
2023-04-11 10:49:31 +0200

    syntax

--- scripts/components/views/RecordView.js
+++ scripts/components/views/RecordView.js
@@ -85,16 +85,20 @@ export default function RecordView() {
     if (eventBus) {
       eventBus.addEventListener('overlay.transcribe.sent', () => {
         // first, wait 500ms before fetching subrecords again
-        setTimeout(() => {
-          fetchSubrecords();
-        }
-        , 3000);
+        setTimeout(
+          () => {
+            fetchSubrecords();
+          },
+          3000,
+        );
         // then, wait 5 seconds before fetching subrecords again.
         // this is to give elasticsearch time to index the new transcription
-        setTimeout(() => {
-          fetchSubrecords();
-        }
-        , 10000);
+        setTimeout(
+          () => {
+            fetchSubrecords();
+          },
+          10000,
+        );
       });
     }
   }, []);
@@ -754,7 +758,7 @@ export default function RecordView() {
                     search: data.archive.archive_id_row,
                     recordtype: 'one_record',
                   }}
-                  />
+                />
 
               </div>
             </div>
