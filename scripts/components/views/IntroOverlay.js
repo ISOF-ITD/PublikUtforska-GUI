77a690e3b55d5e0d770d0a8da3836c8b78314727
Rico Simke <rico.simke@isof.se>
2023-09-20 09:15:27 +0200

    feat: Update IntroOverlay component
    - Fetch content from the API to display in the overlay
    - Add checkbox to choose not to show the overlay again
    - Use 'dangerouslySetInnerHTML' to inject the formatted text
    - Add a Close button to dismiss the overlay
    
    These changes improve the functionality and user experience of the IntroOverlay component.

--- scripts/components/views/IntroOverlay.js
+++ scripts/components/views/IntroOverlay.js
@@ -4,49 +4,25 @@ function IntroOverlay() {
   // useState hook for managing visibility of the overlay
   const [showOverlay, setShowOverlay] = useState(true);
   // useState hook for managing the content fetched from the API
-  const [content, setContent] = useState(`
-    Välkommen till Folke sök! Här kan du söka i ISOFs arkiv och samlingar. 
-    Du kan söka på personer, platser, föremål, händelser och mycket mer.
-    Du kan också söka i Folkebibeln, en samling av bibeltexter på svenska dialekter.
-    Folke sök är en del av projektet Folke, som är ett samarbete mellan
-    Institutet för språk och folkminnen (ISOF) och Kungliga biblioteket (KB).
-
-    Hur fungerar Folke sök?
-    Folke sök är en söktjänst som söker i flera olika databaser samtidigt.
-    När du söker på något i Folke sök får du träffar från flera olika databaser.
-    Du kan sedan klicka dig vidare till den databas som du är intresserad av.
-    Du kan också välja att söka direkt i en databas genom att klicka på dess namn i menyn.
-    Då kommer du till den databasens egen söksida.
-
-    Hur söker jag?
-    Du kan söka på flera olika sätt. Du kan söka på ett ord eller en fras.
-    Du kan också söka på flera ord samtidigt. Då får du träffar som innehåller alla orden.
-    Du kan också söka på en exakt fras genom att sätta citattecken runt orden.
-    Du kan också söka på en exakt fras genom att sätta citattecken runt orden.
-    Du kan också söka på en exakt fras genom att sätta citattecken runt orden.
-
-  `);
+  const [content, setContent] = useState('');
 
   useEffect(() => {
-    // On component mount, we check if the 'hideIntroOverlay' value exists in localStorage
-    // As we are using localStorage, if set, this value will persist indefinitely (until manually cleared)
     const hideIntroOverlay = localStorage.getItem('hideIntroOverlay');
-
+    
     if (hideIntroOverlay) {
-      // If it exists, we set the 'showOverlay' state to false, hence, not displaying the overlay
       setShowOverlay(false);
     } else {
-      // If 'hideIntroOverlay' doesn't exist in localStorage, it means the user hasn't chosen to hide the overlay
-      // We fetch the content from the API to display in the overlay
-      fetch('URL_TILL_SITEVISION_API')
-        .then((response) => response.json())
-        .then((data) => {
-          // Once the data is fetched, we update the 'content' state with the text from the API
-          setContent(data.text); // Replace 'data.text' with the actual key based on API response's structure
-        })
-        .catch((err) => console.error(err));
+      fetch('https://www.isof.se/rest-api/folke-context/start', {
+        method: 'GET',
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        setContent(data.content);
+      })
+      .catch((err) => console.error('Något gick fel:', err));
     }
   }, []);
+  
 
   // Function to handle checkbox change
   const handleCheckboxChange = () => {
@@ -81,13 +57,18 @@ function IntroOverlay() {
         <div>
           {/* The content fetched from the API */}
         </div>
-        {content}
+        <div>
+          {/* Här använder vi 'dangerouslySetInnerHTML' för att injicera den HTML-formaterade texten */}
+          <div dangerouslySetInnerHTML={{ __html: content }} />
+        </div>
         {/* Checkbox to choose not to show the overlay again */}
-        <p/>
+        <p />
         <label htmlFor="hideOverlay">
           <input id="hideOverlay" type="checkbox" onChange={handleCheckboxChange} />
           Visa inte igen
         </label>
+        {/* Adding the Close button here */}
+        <button onClick={handleCloseButtonClick}>Stäng</button>
       </div>
     </div>
   );
