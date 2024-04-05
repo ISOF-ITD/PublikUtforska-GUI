e0858efd93830b28664945a5c5a4ed120a28e78d
Rico Simke <rico.simke@isof.se>
2024-04-05 14:06:32 +0200

    feat: add TXT sitemap generator and update script names
    - Created `create-txt-sitemap.js` to generate a sitemap in text format. This new script fetches document IDs from the updated BASE_URL and lists them in `sitemap.txt`.
    - Updated the base URL for document fetching to `https://garm.isof.se/folkeservice/api/es/documents/...` from a commented-out previous URL.
    - Renamed `create-sitemap.js` to `create-xml-sitemap.js` to clarify its purpose as an XML sitemap generator.
    - Added a new script entry `create-txt-sitemap` to `package.json` and renamed the existing `create-sitemap` script to `create-xml-sitemap` to reflect the changes in script file names.

---
+++ create-txt-sitemap.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+const fs = require('fs');
+
+// Bas-URL för att hämta dokumenten
+// const BASE_URL = 'https://frigg.isof.se/sagendatabas/api/es/documents/?mark_metadata=transcriptionstatus&type=arkiv&categorytypes=tradark&publishstatus=published&has_media=true&transcriptionstatus=published,accession&sort=archive.archive_id_row.keyword&order=asc';
+const BASE_URL = 'https://garm.isof.se/folkeservice/api/es/documents/?mark_metadata=transcriptionstatus&type=arkiv&categorytypes=tradark&publishstatus=published&has_media=true&add_aggregations=false&recordtype=one_record&transcriptionstatus=published';
+// Bas-URL för sitemap-länkarna
+const SITEMAP_BASE_URL = 'https://sok.folke.isof.se/#/records/';
+
+// Funktion för att hämta dokument
+async function fetchDocuments(size, from = 0) {
+    try {
+        console.log(`Fetching documents: Size = ${size}, From = ${from}`);
+        
+        const response = await axios.get(`${BASE_URL}&size=${size}&from=${from}`);
+        return response.data.data;
+    } catch (error) {
+        console.error('Error fetching documents:', error);
+        return [];
+    }
+}
+
+// Funktion för att skapa sitemap i textformat
+async function createTextSitemap() {
+    let from = 0;
+    const size = 500;
+    let sitemapText = '';
+
+    while (true) {
+        const documents = await fetchDocuments(size, from);
+
+        if (documents.length === 0) {
+            break;
+        }
+
+        documents.forEach(doc => {
+            if (doc._source.id) {
+                sitemapText += `${SITEMAP_BASE_URL}${doc._source.id}\n`;
+            }
+        });
+
+        from += size;
+    }
+
+    fs.writeFileSync('sitemap.txt', sitemapText);
+    console.log('Text sitemap created successfully.');
+}
+
+createTextSitemap();
