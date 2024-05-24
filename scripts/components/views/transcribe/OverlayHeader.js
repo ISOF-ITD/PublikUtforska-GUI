9dd6332172429c58561e4f494d27d276909d691f
Rico Simke <rico.simke@isof.se>
2024-05-24 10:17:58 +0200

    add header buttons to transcriptionPageByPageOverlay

--- scripts/components/views/transcribe/OverlayHeader.js
+++ scripts/components/views/transcribe/OverlayHeader.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import config from '../../../config';
 
-const OverlayHeader = ({ recordDetails, handleHideOverlay }) => (
-  <div className="overlay-header">
-    Skriv av
-    {' '}
-    {recordDetails.title || recordDetails.id}
-    <button type="button" title="stäng" className="close-button white" onClick={handleHideOverlay}>Stäng</button>
-  </div>
-);
+import ContributeInfoButton from '../ContributeInfoButton';
+import FeedbackButton from '../FeedbackButton';
+import TranscriptionHelpButton from './TranscriptionHelpButton';
+
+function OverlayHeader({ recordDetails, handleHideOverlay }) {
+  return (
+    <div className="overlay-header">
+      Skriv av
+      {' '}
+      {recordDetails.title || recordDetails.id}
+      <button type="button" title="stäng" className="close-button white" onClick={handleHideOverlay}>Stäng</button>
+      {
+      !config.siteOptions.hideContactButton
+      && <FeedbackButton type="Uppteckning" />
+    }
+      {
+      !config.siteOptions.hideContactButton
+      && <ContributeInfoButton type="Uppteckning" />
+    }
+      {
+      !config.siteOptions.hideContactButton
+      && <TranscriptionHelpButton type="Uppteckning" />
+    }
+    </div>
+  );
+}
 
 export default OverlayHeader;
