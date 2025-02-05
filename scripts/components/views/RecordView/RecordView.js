e21f968b8ccaf1e2f5a917098089427262940a16
Rico <rico.simke@isof.se>
2025-02-05 14:55:12 +0100

    Refactor: Remove lazy loading for components in RecordView

--- scripts/components/views/RecordView/RecordView.js
+++ scripts/components/views/RecordView/RecordView.js
@@ -1,34 +1,33 @@
-import React, { lazy, Suspense, useEffect } from 'react';
+/* eslint-disable react/require-default-props */
 import {
-  Await,
-  useLoaderData,
-  useLocation,
+  Suspense, useEffect, // useState,
+} from 'react';
+import {
+  Await, useLoaderData, useLocation, // useNavigate, //useParams,
 } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { createSearchRoute, createParamsFromRecordRoute } from '../../../utils/routeHelper';
+import AudioItemsElement from './AudioItemsElement';
+import CommentsElement from './CommentsElement';
+import ContentsElement from './ContentsElement';
+import Disclaimer from '../Disclaimer';
+import HeadwordsElement from './HeadwordsElement';
+import License from './License';
 import loaderSpinner from '../../../../img/loader.gif';
+import PdfElement from './PdfElement';
+import PersonItems from './PersonItems';
+import PlaceItems from './PlaceItems';
+import RecordViewFooter from './RecordViewFooter';
+import RecordViewHeader from './RecordViewHeader';
+import RecordViewThumbnails from './RecordViewThumbnails';
+import ReferenceLinks from './ReferenceLinks';
+import SubrecordsElement from './SubrecordsElement';
+import TextElement from './TextElement';
+import RequestToTranscribePrompt from './RequestToTranscribePrompt';
+import TranscriptionPrompt from './TranscriptionPrompt';
+import SimilarRecords from './SimilarRecords';
 import { getTitleText } from '../../../utils/helpers';
 import config from '../../../config';
-
-// Lazy loada komponenter som kanske inte behövs direkt
-const RecordViewHeader = lazy(() => import('./RecordViewHeader'));
-const RecordViewThumbnails = lazy(() => import('./RecordViewThumbnails'));
-const ContentsElement = lazy(() => import('./ContentsElement'));
-const HeadwordsElement = lazy(() => import('./HeadwordsElement'));
-const AudioItemsElement = lazy(() => import('./AudioItemsElement'));
-const PdfElement = lazy(() => import('./PdfElement'));
-const TextElement = lazy(() => import('./TextElement'));
-const CommentsElement = lazy(() => import('./CommentsElement'));
-const License = lazy(() => import('./License'));
-const SubrecordsElement = lazy(() => import('./SubrecordsElement'));
-const PersonItems = lazy(() => import('./PersonItems'));
-const PlaceItems = lazy(() => import('./PlaceItems'));
-const RecordViewFooter = lazy(() => import('./RecordViewFooter'));
-const ReferenceLinks = lazy(() => import('./ReferenceLinks'));
-const Disclaimer = lazy(() => import('../Disclaimer'));
-const RequestToTranscribePrompt = lazy(() => import('./RequestToTranscribePrompt'));
-const TranscriptionPrompt = lazy(() => import('./TranscriptionPrompt'));
-const SimilarRecords = lazy(() => import('./SimilarRecords'));
 
 function RecordView({ mode = 'material' }) {
   const { results: resultsPromise } = useLoaderData();
@@ -47,59 +46,80 @@ function RecordView({ mode = 'material' }) {
   };
 
   useEffect(() => {
-    document.title = config.siteTitle;
+    document.title = config.siteTitle; // Justera efter datan om så behövs
   }, []);
 
   return (
     <div className="container">
-      <Suspense fallback={
+      <Suspense fallback={(
         <>
           <div className="container-header" style={{ height: 130 }} />
           <div className="container-body">
             <img src={loaderSpinner} alt="Hämtar data" />
           </div>
         </>
-      }>
+      )}
+      >
         <Await
           resolve={resultsPromise}
-          errorElement={<div>Det uppstod ett fel vid laddning av posten.</div>}
+          errorElement={
+            <div>Det uppstod ett fel vid laddning av posten.</div>
+          }
         >
-          {([highlightData, { _source: data }, { data: subrecordsCount }]) => {
+          {([
+            highlightData,
+            { _source: data },
+            { data: subrecordsCount },
+          ]) => {
+            // i records[0] finns highlights!
             if (!data) return <div>Posten finns inte.</div>;
+            // const titleText = getTitleText(data);
+            // const country = data.archive?.country || 'unknown';
+            // const mediaItems = data.media || [];
+            // const pages = getPages(data);
             document.title = `${getTitleText(data, 0, 0)} - ${config.siteTitle}`;
             return (
               <article>
-                <Suspense fallback={<div>Laddar...</div>}>
-                  <RecordViewHeader data={data} subrecordsCount={subrecordsCount} location={location} />
-                </Suspense>
+                <RecordViewHeader
+                  data={data}
+                  subrecordsCount={subrecordsCount}
+                  location={location}
+                />
                 <div className="container-body">
-                  <Suspense fallback={<div>Laddar information...</div>}>
-                    <Disclaimer />
-                    <TranscriptionPrompt data={data} />
-                    <RequestToTranscribePrompt data={data} />
-                    <RecordViewThumbnails data={data} mediaImageClickHandler={mediaImageClickHandler} />
-                    <ContentsElement data={data} />
-                    <HeadwordsElement data={data} />
-                    <AudioItemsElement data={data} />
-                    <PdfElement data={data} />
-                    <TextElement data={data} highlightData={highlightData} mediaImageClickHandler={mediaImageClickHandler} />
-                    <CommentsElement data={data} />
-                    <div className="row">
-                      <div className="eight columns">
-                        <ReferenceLinks data={data} />
-                      </div>
-                      <div className="four columns">
-                        <License data={data} />
-                      </div>
+                  <Disclaimer />
+                  <TranscriptionPrompt data={data} />
+                  <RequestToTranscribePrompt data={data} />
+                  <RecordViewThumbnails
+                    data={data}
+                    mediaImageClickHandler={mediaImageClickHandler}
+                  />
+                  <ContentsElement data={data} />
+                  <HeadwordsElement data={data} />
+                  <AudioItemsElement data={data} />
+                  <PdfElement data={data} />
+                  <TextElement
+                    data={data}
+                    highlightData={highlightData}
+                    mediaImageClickHandler={mediaImageClickHandler}
+                  />
+                  <CommentsElement data={data} />
+                  <div className="row">
+                    <div className="eight columns">
+                      <ReferenceLinks data={data} />
                     </div>
-                    <SubrecordsElement data={data} subrecordsCount={subrecordsCount} mode={mode} />
-                    <PersonItems data={data} routeParams={routeParams} />
-                    <PlaceItems data={data} routeParams={routeParams} />
-                    <hr />
-                    <SimilarRecords data={data} />
-                    <hr />
-                    <RecordViewFooter data={data} />
-                  </Suspense>
+                    <div className="four columns">
+                      <License data={data} />
+                    </div>
+                  </div>
+                  <SubrecordsElement data={data} subrecordsCount={subrecordsCount} mode={mode} />
+                  <PersonItems data={data} routeParams={routeParams} />
+                  <PlaceItems data={data} routeParams={routeParams} />
+                  <hr />
+                  <SimilarRecords data={data} />
+                  <hr />
+                  <RecordViewFooter data={data} />
+
+                  {/* <MetadataItems data={data} /> */}
                 </div>
               </article>
             );
@@ -110,8 +130,8 @@ function RecordView({ mode = 'material' }) {
   );
 }
 
+export default RecordView;
+
 RecordView.propTypes = {
   mode: PropTypes.string,
 };
-
-export default RecordView;
