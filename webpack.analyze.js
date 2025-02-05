c48a3945c426a51449f63bfa90166c4250638ef8
Rico Simke <rico.simke@isof.se>
2025-02-05 10:00:28 +0100

    Add webpack-bundle-analyzer and create analysis configuration

---
+++ webpack.analyze.js
@@ -0,0 +1,19 @@
+const { merge } = require('webpack-merge');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  plugins: [new BundleAnalyzerPlugin()],
+
+  mode: 'production',
+
+  // tells webpack to split the code
+  // into multiple files
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+
+});
