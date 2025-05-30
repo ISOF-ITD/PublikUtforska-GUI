b2a7854befd4fb59c231a654607172cda3b0fe74
Liudmila Kazakbaeva <83576552+liudmilitas@users.noreply.github.com>
2025-05-30 17:14:22 +0200

    Use rgb color values instead of HEX in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        /* Icke aktiv knapp: (om fungerar alternativt Text alternativfärg) */
-        "lighter-isof": "#3FD695",
+        /* Icke aktiv knapp: (om fungerar alternativt Text alternativfärg) #3FD695 */
+        "lighter-isof": "rgb(63, 214, 149)",
 
-        /* Profilfärg - exempel på en custom classnamn - text-isof, bg-isof */
-        isof: "#005462",
+        /* Profilfärg - exempel på en custom classnamn - text-isof, bg-isof #005462 */
+        isof: "rgb(0, 84, 98)",
 
-        /* Aktiv knapp + knapp-hover-over */
-        "darker-isof": "#1c3f49",
+        /* Aktiv knapp + knapp-hover-over #1c3f49 */
+        "darker-isof": "rgb(28, 63, 73)",
       },
     },
   },
