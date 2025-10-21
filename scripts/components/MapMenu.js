f5381707e8acc60bba470b8605b0207f52f2c683
Liudmila Kazakbaeva <83576552+liudmilitas@users.noreply.github.com>
2025-10-21 17:11:25 +0200

    Refactor MapMenu puffar styles and structure

--- scripts/components/MapMenu.js
+++ scripts/components/MapMenu.js
@@ -183,8 +183,8 @@ export default function MapMenu({
         </button>
       </div>
 
-      <div className="puffar">
-        <div className="statistics puff">
+      <div className="flex-1 overflow-y-auto p-4 mb-3 flex flex-col rounded-xl items-stretch h-full bg-white">
+        <div className="statistics">
           <StatisticsContainer />
 
           <h3>Senast avskrivna uppteckningar</h3>
