b76b56880ee569da846dcf5e3aff162b38fc95dc
Liudmila Kazakbaeva <83576552+liudmilitas@users.noreply.github.com>
2025-10-28 11:13:30 +0100

    Refactor map markers to use Font Awesome icons

--- scripts/utils/mapHelper.js
+++ scripts/utils/mapHelper.js
@@ -1,202 +1,164 @@
-// import turfInside from '@turf/boolean-point-in-polygon';
-import L, { icon, tileLayer, bounds } from 'leaflet';
-import mapMarkerBlueLocation from '../../img/map-marker-blue-location.png';
-import mapMarkerBlueHighlighted from '../../img/map-marker-blue-highlighted.png';
-import mapMarkerShadow from '../../img/map-marker-shadow.png';
-import markerBlue from '../../img/marker-blue.png';
-import markerRed from '../../img/marker-red.png';
+import L, { divIcon, tileLayer, bounds } from "leaflet";
+import { icon as faIcon } from "@fortawesome/fontawesome-svg-core";
+import { faLocationDot, faCircle } from "@fortawesome/free-solid-svg-icons";
+
+// helper to turn a FA definition into an HTML string sized & colored
+function faSvg(
+  definition,
+  { color = "#3D84D4", size = 28, extraClasses = "" } = {}
+) {
+  // Font Awesome returns an SVG string in .html[0]
+  const svg = faIcon(definition, { classes: extraClasses }).html[0];
+
+  // Soft white outline (halo) + drop shadow.
+  const isPin = definition?.iconName === "location-dot";
+  const halo = isPin
+    ? "drop-shadow(0 0 1px #fff) drop-shadow(0 0 2px #fff) drop-shadow(0 0 3px #fff) "
+    : "";
+
+  // Size via font-size because FA SVGs are 1em by default; color inherits as currentColor
+  return `<span class="fa-marker" style="display:inline-block; line-height:1; font-size:${size}px; color:${color}; filter:${halo}drop-shadow(0 2px 3px rgba(0,0,0,.45));">${svg}</span>`;
+}
+
+// create a pin-style DivIcon (tip at bottom center)
+function makeFaPin({ color = "#3D84D4", size = 28, extraClasses = "" } = {}) {
+  const anchorX = Math.round(size / 2);
+  const anchorY = Math.round(size * 0.92); // close to the pin tip
+  return divIcon({
+    html: faSvg(faLocationDot, { color, size, extraClasses }),
+    className: "fa-leaflet-marker", // remove default Leaflet divIcon styling via CSS below
+    iconSize: [size, size], // clickable area
+    iconAnchor: [anchorX, anchorY], // the "tip" of the pin
+    popupAnchor: [0, -anchorY + 6], // popup just above the tip
+  });
+}
+
+// create a round 27x27 marker
+function makeFaDot({ color = "#3D84D4", size = 27, extraClasses = "" } = {}) {
+  const half = Math.round(size / 2);
+  return divIcon({
+    html: faSvg(faCircle, { color, size, extraClasses }),
+    className: "fa-leaflet-marker",
+    iconSize: [size, size],
+    iconAnchor: [half, half],
+    popupAnchor: [0, -half],
+  });
+}
 
 export default {
-  markerIcon: icon({
-    iconUrl: mapMarkerBlueLocation,
-    shadowUrl: mapMarkerShadow,
-
-    iconSize: [28, 36], // Size of the icon
-    shadowSize: [41, 41], // Size of the shadow
-    iconAnchor: [14, 35], // Point of the icon corresponding to marker's location
-    shadowAnchor: [12, 40], // Same for the shadow
-    popupAnchor: [-1, -15], // Point from which the popup should open relative to the iconAnchor
-  }),
-
-  markerIconHighlighted: icon({
-    iconUrl: mapMarkerBlueHighlighted,
-    shadowUrl: mapMarkerShadow,
-
-    iconSize: [28, 36],
-    shadowSize: [41, 41],
-    iconAnchor: [14, 35],
-    shadowAnchor: [12, 40],
-    popupAnchor: [-1, -15],
-  }),
-
-  blueIcon: icon({
-    iconUrl: markerBlue,
-    iconSize: [27, 27],
-    iconAnchor: [15, 15],
-    popupAnchor: [0, 0],
-  }),
-
-  redIcon: icon({
-    iconUrl: markerRed,
-    iconSize: [27, 27],
-    iconAnchor: [15, 15],
-    popupAnchor: [0, 0],
-  }),
+  // Pin markers
+  markerIcon: makeFaPin({ color: "#3D84D4", size: 28 }), // default blue
+  markerIconHighlighted: makeFaPin({ color: "#0ea5e9", size: 30 }),
+  // Round markers
+  blueIcon: makeFaDot({ color: "#1d4ed8", size: 27 }),
+  redIcon: makeFaDot({ color: "#dc2626", size: 27 }),
 
   tileLayers: [
-    /*
-		// Lantmäteriet topografisk karta SWEREF99
-    Not used now as SWEREF99 needs reprojection of other map layers
-		{
-			label: 'Lantmäteriet topografisk karta (SWEREF99)',
-			url: 'https://garm.isof.se/folkeservice/api/lm_proxy/{z}/{y}/{x}.png',
-			options: {
-				attribution: '&copy; <a href="https://www.lantmateriet.se/en/">Lantmäteriet</a> Topografisk Webbkarta Visning',
-				crossOrigin: true,
-			}
-		},
-		*/
-    // Lantmäteriet topografisk karta epsg3857 WGS 84
-    // It seems maxZoom="17": Can be set as prop to MapBase component
     {
-      label: 'Lantmäteriet topografisk karta',
-      url: 'https://garm.isof.se/folkeservice/api/lm_epsg3857_proxy/{z}/{y}/{x}.png',
+      label: "Lantmäteriet topografisk karta",
+      url: "https://garm.isof.se/folkeservice/api/lm_epsg3857_proxy/{z}/{y}/{x}.png",
       options: {
-        attribution: '&copy; <a href="https://www.lantmateriet.se/en/">Lantmäteriet</a>',
+        attribution:
+          '&copy; <a href="https://www.lantmateriet.se/en/">Lantmäteriet</a>',
         crossOrigin: true,
       },
     },
     {
-      label: 'Lantmäteriet topografisk karta nedtonad',
-      url: 'https://garm.isof.se/folkeservice/api/lm_nedtonad_epsg3857_proxy/{z}/{y}/{x}.png',
+      label: "Lantmäteriet topografisk karta nedtonad",
+      url: "https://garm.isof.se/folkeservice/api/lm_nedtonad_epsg3857_proxy/{z}/{y}/{x}.png",
       options: {
-        attribution: '&copy; <a href="https://www.lantmateriet.se/en/">Lantmäteriet</a>',
+        attribution:
+          '&copy; <a href="https://www.lantmateriet.se/en/">Lantmäteriet</a>',
         crossOrigin: true,
       },
     },
     {
-      label: 'Open Street Map Mapnik',
-      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      label: "Open Street Map Mapnik",
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       options: {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      },
-    },
-    /*
-		{
-			label: 'Lantmäteriet', // Lantmäteriet open
-			url: 'https://api.lantmateriet.se/open/topowebb-ccby/v1/wmts/token/36c696a2a831dcf8aa64c192b55d4d/1.0.0/topowebb/default/3006/{z}/{y}/{x}.png',
-			options: {
-				maxZoom: 9,
-				minZoom: 0,
-				attribution: '&copy; <a href="https://www.lantmateriet.se/en/">Lantmäteriet</a> Topografisk Webbkarta Visning'
-			}
-		},
-		*/
-    /*
-			Token now needed for API:
-			https://wiki.openstreetmap.org/wiki/OpenMapSurfer
-		{
-			label: 'Open Map Surfer',
-			url: 'https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}',
-			options: {
-				attribution: 'Imagery from <a href="https://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-			}
-		},
-    // Lantmäteriet ortofoto karta: Old config not used
-		{
-			label: 'Lantmäteriet ortofoto karta',
-			url: 'https://garm-test.isof.se/folkeservice/api/lm_orto_proxy/',
-			//url: 'http://ifsf0001:h52w0OaX5eW@maps.lantmateriet.se/ortofoto/wms/v1.3',
-			options: {
-				attribution: '&copy; <a href="https://www.lantmateriet.se/en/">Lantmäteriet</a> Topografisk Ortofoto Visning',
-				crossOrigin: true,
-			}
-		},
-		*/
-    {
-      label: 'ESRI World Imagery',
-      url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-      options: {
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       },
     },
     {
-      label: 'ESRI Gray',
-      url: 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
+      label: "ESRI World Imagery",
+      url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
       options: {
-        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+        attribution:
+          "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
       },
     },
-    /*
-		{
-			label: 'Göteborgs og Bohus län',
-			url: 'http://localhost/ISOF/Sagenkarta-GUI/www/tiles/gbl/{z}/{x}/{y}.png',
-			options: {
-				attribution: 'Data &copy; Lantmäteriet'
-			}
-		}
-*/
+    {
+      label: "ESRI Gray",
+      url: "https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}",
+      options: {
+        attribution: "Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ",
+      },
+    },
   ],
 
   overlayTileLayers: [
     // Historiska Ortofoto LM
     {
       isWms: true,
-      label: 'Lantmäteriet Ortofoto 1960',
-      url: 'https://garm.isof.se/folkeservice/api/lm_historto_proxy',
-      layers: 'OI.Histortho_60',
+      label: "Lantmäteriet Ortofoto 1960",
+      url: "https://garm.isof.se/folkeservice/api/lm_historto_proxy",
+      layers: "OI.Histortho_60",
       TILED: true,
-      TILESORIGIN: '-2238400, 5287200',
+      TILESORIGIN: "-2238400, 5287200",
       ISBASELAYER: true,
       hidden: true,
       maxZoom: 17,
       minZoom: 6,
       options: {
-        attribution: '&copy; <a href="https://www.lantmateriet.se/sv/Kartor-och-geografisk-information/geodataprodukter/produktlista/historiska-ortofoton-visning/">Lantmäteriverket</a> Ortofoto 1960',
+        attribution:
+          '&copy; <a href="https://www.lantmateriet.se/sv/Kartor-och-geografisk-information/geodataprodukter/produktlista/historiska-ortofoton-visning/">Lantmäteriverket</a> Ortofoto 1960',
         crossOrigin: true,
       },
     },
     // Ortofoto LM
     {
       isWms: true,
-      label: 'Lantmäteriet Ortofoto',
-      url: 'https://garm.isof.se/folkeservice/api/lm_orto_proxy',
-      layers: 'Ortofoto_0.5,Ortofoto_0.4,Ortofoto_0.25,Ortofoto_0.16',
+      label: "Lantmäteriet Ortofoto",
+      url: "https://garm.isof.se/folkeservice/api/lm_orto_proxy",
+      layers: "Ortofoto_0.5,Ortofoto_0.4,Ortofoto_0.25,Ortofoto_0.16",
       // layers: "Ortofoto_0.5%2COrtofoto_0.4%2COrtofoto_0.25%2COrtofoto_0.16",
       TILED: true,
-      TILESORIGIN: '-2238400, 5287200',
+      TILESORIGIN: "-2238400, 5287200",
       ISBASELAYER: true,
       hidden: true,
       maxZoom: 17,
       minZoom: 6,
       options: {
-        attribution: '&copy; <a href="https://www.lantmateriet.se/sv/Kartor-och-geografisk-information/geodataprodukter/produktlista/ortofoto-visning/">Lantmäteriverket</a> Ortofoto',
+        attribution:
+          '&copy; <a href="https://www.lantmateriet.se/sv/Kartor-och-geografisk-information/geodataprodukter/produktlista/ortofoto-visning/">Lantmäteriverket</a> Ortofoto',
         crossOrigin: true,
       },
     },
     // Socken RAÄ
     {
       isWms: true,
-      label: 'Sockengränser',
-      url: 'https://karta.raa.se/geo/arkreg_v1.0/wms',
-      layers: 'socken',
+      label: "Sockengränser",
+      url: "https://karta.raa.se/geo/arkreg_v1.0/wms",
+      layers: "socken",
       TILED: true,
-      TILESORIGIN: '-2238400, 5287200',
+      TILESORIGIN: "-2238400, 5287200",
       ISBASELAYER: false,
       hidden: false,
       maxZoom: 17,
       minZoom: 11,
       options: {
-        attribution: '&copy; <a href="https://www.raa.se">Riksankikvarieämbetet</a> Sockenkarta',
+        attribution:
+          '&copy; <a href="https://www.raa.se">Riksankikvarieämbetet</a> Sockenkarta',
         crossOrigin: true,
       },
     },
     // Generalstabskartan Länsstyrelsen
     {
       isWms: true,
-      label: 'Generalstabskartan',
-      url: 'https://ext-geodata-raster.lansstyrelsen.se/arcgis/services/RasterNationellt/lst_ext_generalstabskartan/ImageServer/WMSServer',
-      layers: 'lst_ext_generalstabskartan',
+      label: "Generalstabskartan",
+      url: "https://ext-geodata-raster.lansstyrelsen.se/arcgis/services/RasterNationellt/lst_ext_generalstabskartan/ImageServer/WMSServer",
+      layers: "lst_ext_generalstabskartan",
       //TILED: true,
       //TILESORIGIN: '-2238400, 5287200',
       ISBASELAYER: false,
@@ -204,16 +166,17 @@ export default {
       maxZoom: 17,
       minZoom: 3,
       options: {
-        attribution: '&copy;<a href="https://www.lansstyrelsen.se/">Länsstyrelsen</a> Generalstabskartan',
+        attribution:
+          '&copy;<a href="https://www.lansstyrelsen.se/">Länsstyrelsen</a> Generalstabskartan',
         crossOrigin: true,
       },
     },
     // Häradsekonomiska kartan Länsstyrelsen
     {
       isWms: true,
-      label: 'Häradsekonomiska kartan',
-      url: 'https://ext-geodata-raster.lansstyrelsen.se/arcgis/services/RasterNationellt/lst_ext_haradsekonomiska_kartan_temp/ImageServer/WMSServer',
-      layers: 'lst_ext_haradsekonomiska_kartan_temp',
+      label: "Häradsekonomiska kartan",
+      url: "https://ext-geodata-raster.lansstyrelsen.se/arcgis/services/RasterNationellt/lst_ext_haradsekonomiska_kartan_temp/ImageServer/WMSServer",
+      layers: "lst_ext_haradsekonomiska_kartan_temp",
       //TILED: true,
       //TILESORIGIN: '-2238400, 5287200',
       ISBASELAYER: false,
@@ -221,16 +184,17 @@ export default {
       maxZoom: 17,
       minZoom: 6,
       options: {
-        attribution: '&copy;<a href="https://www.lansstyrelsen.se/">Länsstyrelsen</a> Häradsekonomiska kartan',
+        attribution:
+          '&copy;<a href="https://www.lansstyrelsen.se/">Länsstyrelsen</a> Häradsekonomiska kartan',
         crossOrigin: true,
       },
     },
     // Ekonomiska kartan Länsstyrelsen
     {
       isWms: true,
-      label: 'Ekonomiska kartan',
-      url: 'https://ext-geodata-raster.lansstyrelsen.se/arcgis/services/RasterNationellt/lst_ext_ekonomiska_kartan/ImageServer/WMSServer',
-      layers: 'lst_ext_ekonomiska_kartan',
+      label: "Ekonomiska kartan",
+      url: "https://ext-geodata-raster.lansstyrelsen.se/arcgis/services/RasterNationellt/lst_ext_ekonomiska_kartan/ImageServer/WMSServer",
+      layers: "lst_ext_ekonomiska_kartan",
       //TILED: true,
       //TILESORIGIN: '-2238400, 5287200',
       ISBASELAYER: false,
@@ -238,7 +202,8 @@ export default {
       maxZoom: 17,
       minZoom: 3,
       options: {
-        attribution: '&copy;<a href="https://www.lansstyrelsen.se/">Länsstyrelsen</a> Ekonomiska kartan',
+        attribution:
+          '&copy;<a href="https://www.lansstyrelsen.se/">Länsstyrelsen</a> Ekonomiska kartan',
         crossOrigin: true,
       },
     },
@@ -246,10 +211,8 @@ export default {
 
   createLayers() {
     const ret = {};
-
     for (let i = 0; i < this.tileLayers.length; i++) {
-      var newLayer;
-
+      let newLayer;
       if (this.tileLayers[i].isWms) {
         newLayer = tileLayer.wms(this.tileLayers[i].url, {
           layers: this.tileLayers[i].layerName,
@@ -258,7 +221,10 @@ export default {
           attribution: this.tileLayers[i].attribution,
         });
       } else {
-        newLayer = tileLayer(this.tileLayers[i].url, this.tileLayers[i].options);
+        newLayer = tileLayer(
+          this.tileLayers[i].url,
+          this.tileLayers[i].options
+        );
       }
       ret[this.tileLayers[i].label] = newLayer;
     }
@@ -269,14 +235,13 @@ export default {
     const ret = {};
     if (this.overlayTileLayers) {
       for (let i = 0; i < this.overlayTileLayers.length; i++) {
-        var newLayer;
-
+        let newLayer;
         if (this.overlayTileLayers[i].isWms) {
           newLayer = tileLayer.wms(this.overlayTileLayers[i].url, {
             layers: this.overlayTileLayers[i].layers,
             minZoom: this.overlayTileLayers[i].minZoom || 3,
             maxZoom: this.overlayTileLayers[i].maxZoom,
-            format: 'image/png',
+            format: "image/png",
             transparent: true,
             hidden: this.overlayTileLayers[i].hidden,
             attribution: this.overlayTileLayers[i].options.attribution,
@@ -285,7 +250,10 @@ export default {
             TILESORIGIN: this.overlayTileLayers[i].TILESORIGIN,
           });
         } else {
-          newLayer = tileLayer(this.overlayTileLayers[i].url, this.overlayTileLayers[i].options);
+          newLayer = tileLayer(
+            this.overlayTileLayers[i].url,
+            this.overlayTileLayers[i].options
+          );
         }
         ret[this.overlayTileLayers[i].label] = newLayer;
       }
@@ -293,28 +261,17 @@ export default {
     return ret;
   },
 
-  /*
-	inSweden(lat, lng) {
-		return (turfInside({
-			type: 'Feature',
-			geometry: {
-				type: 'Point',
-				coordinates: [lng, lat]
-			}
-		}, swedenBorder.features[0]));
-	},
-*/
   getSweref99crs() {
     const crs = new L.Proj.CRS(
-      'EPSG:3006',
-      '+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
+      "EPSG:3006",
+      "+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs",
       {
         resolutions: [
           4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1, 0.5,
         ],
-        origin: [-1200000.000000, 8500000.000000],
+        origin: [-1200000.0, 8500000.0],
         bounds: bounds([-1200000.0, 8500000.0], [4305696.0, 2994304.0]),
-      },
+      }
     );
     return crs;
   },
