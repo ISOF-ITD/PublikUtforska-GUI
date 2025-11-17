ae38a84711fc8e8aa01b50b2861748411246960a
Liudmila Kazakbaeva <83576552+liudmilitas@users.noreply.github.com>
2025-11-17 10:34:27 +0100

    Disable MapMenu pointer events when collapsed

--- scripts/components/MapMenu.js
+++ scripts/components/MapMenu.js
@@ -217,8 +217,10 @@ export default function MapMenu({
       aria-hidden={!expanded}
       className={classNames(
         "bg-isof flex flex-col print:hidden absolute top-0 bottom-0 w-96 border-r-2 border-white !z-[1201]",
-        "pt-5 px-5 items-center transition-all duration-300 ease-in-out pointer-events-auto max-sm:box-border max-sm:w-full max-sm:p-2.5",
-        expanded ? "left-0" : "-left-[500px]"
+        "pt-5 px-5 items-center transition-all duration-300 ease-in-out max-sm:box-border max-sm:w-full max-sm:p-2.5",
+        expanded
+          ? "left-0 pointer-events-auto"
+          : "-left-[500px] pointer-events-none"
       )}
     >
       {/* <SurveyLink />  enable when needed */}
