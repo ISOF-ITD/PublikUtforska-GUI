8f8542f65264da26d0e3285596b9214948581b63
Per Låås <per.laas@isof.se>
2025-12-09 09:13:07 +0100

    switch api to prod

--- switch-api-2-localhost.sh
+++ switch-api-2-localhost.sh
@@ -1,4 +1,6 @@
 find www -name '*.js' | xargs grep -l 'https://garm.isof.se/folkeservice/' | xargs sed -i.bak 's/https:\/\/garm.isof.se\/folkeservice/http:\/\/localhost:8000\//g'
 #find www -name '*.js' | xargs grep -l 'https://garm.isof.se/folkeservice/' | xargs sed -i.bak 's|https://garm.isof.se/folkeservice/|http://localhost:8000/|g'
+#find www -name '*.js' | xargs grep -l 'https://garm-test.isof.se/folkeservice/' | xargs sed -i.bak 's/https:\/\/garm-test.isof.se\/folkeservice/https:\/\/garm.isof.se\/folkeservice/g'
+#find www -name '*.js' | xargs grep -l 'https://garm-test.isof.se/folkeservice/' | xargs sed -i.bak 's|https://garm-test.isof.se/folkeservice/|https:\/\/garm.isof.se\/folkeservice|g'
 #sed www/bndl.c23ef7ffa56b703d4212.js 's|https://garm.isof.se/folkeservice/|http://localhost:8000/|g' > www/bndl.c23ef7ffa56b703d4212.js.new
 
