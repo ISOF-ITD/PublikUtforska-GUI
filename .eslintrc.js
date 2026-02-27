80b36b9fa5365f8b263e565c4ea8def1ec1a811f
Rico <rico.simke@isof.se>
2026-02-27 20:58:06 +0100

    update ESLint configuration to include jsx-a11y plugin and clean up rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:react/recommended',
     'airbnb',
     'plugin:react/jsx-runtime',
+    'plugin:jsx-a11y/recommended',
   ],
   overrides: [
   ],
@@ -20,23 +21,22 @@ module.exports = {
   },
   plugins: [
     'react',
+    'jsx-a11y',
   ],
   rules: {
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-uses-vars': 'error',
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/no-danger': 'off',
-    // Disable requirement for default props as we do not use it 
-    'react/require-default-props': 'off',
-    'react/forbid-prop-types': [1, { forbid: [] }],
-    // New rule: No import av default export "React" från "react"
+    'react/react-in-jsx-scope': 'off', // Do not require React to be in scope when using JSX, as of React 17
+    'react/jsx-uses-vars': 'error', // Ensure variables used in JSX are marked as used
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], // Allow JSX in .js files
+    'react/no-danger': 'off', // Allow usage of dangerouslySetInnerHTML when necessary
+    'react/require-default-props': 'off', // Disable requirement for default props as we do not use it
+    'react/forbid-prop-types': [1, { forbid: [] }], // Allow usage of PropTypes.object and PropTypes.array
+    // Forbid import of default export React from "react"
     // See: https://legacy.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
     'no-restricted-imports': ['error', {
       paths: [{
         name: 'react',
         importNames: ['default'],
-        message: 'Import av default export "React" från "react" är inte nödvändigt sedan React 17.',
+        message: 'Import av default export React från \'react\' är inte nödvändigt sedan React 17.',
       }],
     }],
   },
