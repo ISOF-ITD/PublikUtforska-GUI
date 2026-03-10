5617680e41a8c9c350c8e22b15da24dc5b7a480c
Rico Simke <rico.simke@isof.se>
2026-03-10 20:17:29 +0100

    Add mobile route header logos and sizing

--- scripts/components/views/HeaderActions.js
+++ scripts/components/views/HeaderActions.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 import { faArrowLeft, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { l } from '../../lang/Lang';
 import { IconButton } from '../IconButton';
+import folkeWhiteLogo from '../../../img/folke-white.svg';
+import IsofLogoWhite from '../../../img/Isof_logotyp_vit.png';
 
-const baseRailClassName = 'absolute right-8 top-0 z-[3100] flex items-start gap-2';
+const baseRailClassName = 'absolute left-4 right-4 top-0 z-[3100] flex items-center justify-between lg:left-auto lg:right-8 lg:justify-start lg:items-start';
 const backSlotClassName = 'h-6 w-6 flex items-start justify-center';
 
 export default function HeaderActions({
@@ -21,28 +23,35 @@ export default function HeaderActions({
 
   return (
     <div className={classNames(baseRailClassName, className)}>
-      <div className={backSlotClassName}>
-        {showBack && (
-          <IconButton
-            icon={faArrowLeft}
-            label={l('G\u00e5 tillbaka')}
-            tone="light"
-            onClick={onBack}
-            size="sm"
-            className={classNames('!mt-0', lightFocusClassName)}
-            data-route-popup-header-action="back"
-          />
-        )}
-      </div>
-      <IconButton
-        icon={faXmark}
-        label={l('St\u00e4ng')}
-        tone={closeTone}
-        onClick={onClose}
-        size="sm"
-        className={classNames('!mt-0', closeFocusClassName)}
-        data-route-popup-header-action="close"
+      <img
+        src={folkeWhiteLogo}
+        alt={l('Folkelogga')}
+        className="h-8 w-auto pt-2 md:pt-1 lg:hidden"
       />
+      <div className="flex items-start gap-2">
+        <div className={backSlotClassName}>
+          {showBack && (
+            <IconButton
+              icon={faArrowLeft}
+              label={l('G\u00e5 tillbaka')}
+              tone="light"
+              onClick={onBack}
+              size="sm"
+              className={classNames('!mt-0', lightFocusClassName)}
+              data-route-popup-header-action="back"
+            />
+          )}
+        </div>
+        <IconButton
+          icon={faXmark}
+          label={l('St\u00e4ng')}
+          tone={closeTone}
+          onClick={onClose}
+          size="sm"
+          className={classNames('!mt-0', closeFocusClassName)}
+          data-route-popup-header-action="close"
+        />
+      </div>
     </div>
   );
 }
