0390e53dd13de7ca6ae7a1addfac0e2e6e7abe2d
Rico <rico.simke@isof.se>
2026-06-22 11:14:33 +0200

    Add comment to clarify source-map configuration in production settings

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,6 +7,7 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
 
   mode: 'production',
+  // activate source-map for debugging, but disable for production to get the best performance
   // devtool: 'source-map', // external source maps for production
   devtool: false,
 
