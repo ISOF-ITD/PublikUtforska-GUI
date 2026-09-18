435ce833ee5b72c3d84c882e6acae1f80a904b7d
Rico <rico.simke@isof.se>
2026-09-18 10:27:29 +0200

    Replace og-screenshot image format from PNG to JPG and update webpack configuration accordingly

--- webpack.common.js
+++ webpack.common.js
@@ -110,7 +110,7 @@ module.exports = {
       patterns: [
         { from: 'varning*', to: '' }, // Copy varning-files to www-folder
         { from: 'googlef01bc830ea17f73e.html', to: '' }, // Copy google site verification to www-folder
-        { from: 'img/og-screenshot.png', to: 'img' }, // Copy og-screenshot.png to www/img-folder
+        { from: 'img/og-screenshot.jpg', to: 'img' }, // Copy og-screenshot.jpg to www/img-folder
         // Not needed because sitemaps are generated on server
         // { from: 'sitemap*.xml', to: '' }, // Copy all individual sitemaps (sitemap_0.txt, sitemap_1.txt, etc.)
       ],
